60233c424572b3c4c31295ffcb217d7dc781402e
Dorin Potorac <dorin.potorac@visma.com>
2020-04-24 15:46:48 +0300

    Add watcher component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WatcherComponent } from './watcher/watcher.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WatcherComponent
   ],
   imports: [
     BrowserModule,
